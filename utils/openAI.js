c6d0b06f5b293144f9d784c5fbaf634d47f9d768
julien wagentrutz <julienwagentrutz@MacBook-Pro-de-julien.local>
2023-05-21 19:28:58 +0200

    Update api for vercel

--- utils/openAI.js
+++ utils/openAI.js
@@ -5,5 +5,6 @@ export default function chatCompletion(_messages, _options = {}) {
 	const config = {
 		model: "gpt-3.5-turbo",
 	}
-	return openai.createChatCompletion({...config,messages: [..._messages],..._options});
+	return "gjeigz"
+	// return openai.createChatCompletion({...config,messages: [..._messages],..._options});
 }
