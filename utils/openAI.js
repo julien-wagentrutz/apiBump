b1d74afae96a6d1d3c3bb1e99396a440780236af
julien wagentrutz <julienwagentrutz@MacBook-Pro-de-julien.local>
2023-05-21 19:25:46 +0200

    Update api for vercel

--- utils/openAI.js
+++ utils/openAI.js
@@ -5,8 +5,5 @@ export default async function chatCompletion(_messages, _options = {}) {
 	const config = {
 		model: "gpt-3.5-turbo",
 	}
-	setTimeout(async() => {
-		const response = await openai.createChatCompletion({...config,messages: [..._messages],..._options});
-		return response
-	}, 30000)
+	return await openai.createChatCompletion({...config,messages: [..._messages],..._options});
 }
