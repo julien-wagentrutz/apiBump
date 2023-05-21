f16f2442bb1ab72fbc02d06b988032a2ddae94dc
julien wagentrutz <julienwagentrutz@MacBook-Pro-de-julien.local>
2023-05-21 18:32:53 +0200

    config vercel

--- utils/openAI.js
+++ utils/openAI.js
@@ -5,10 +5,8 @@ export default async function chatCompletion(_messages, _options = {}) {
 	const config = {
 		model: "gpt-3.5-turbo",
 	}
-	return new Promise((resolve) => {
-		setTimeout(async() => {
-			const response = await openai.createChatCompletion({...config,messages: [..._messages],..._options});
-			resolve(response.data.choices[0]);
-		}, 30000)
-	});
+	setTimeout(async() => {
+		const response = await openai.createChatCompletion({...config,messages: [..._messages],..._options});
+		return response
+	}, 30000)
 }
