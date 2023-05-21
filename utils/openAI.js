7861b71e386897deb067d45f01b0fb06694ee48f
julien wagentrutz <julienwagentrutz@MacBook-Pro-de-julien.local>
2023-05-21 22:20:56 +0200

    Update api for vercel

--- utils/openAI.js
+++ utils/openAI.js
@@ -1,6 +1,15 @@
-import { openai } from "../config/openAI.js"
+import { Configuration, OpenAIApi } from "openai";
+import * as dotenv from 'dotenv'
 
 export default function chatCompletion(_messages, _options = {}) {
+
+	dotenv.config()
+	
+	const configuration = new Configuration({
+	  apiKey: process.env.OPENAI_API_KEY,
+	});
+	
+	const openai = new OpenAIApi(configuration);
 
 	const config = {
 		model: "gpt-3.5-turbo",
