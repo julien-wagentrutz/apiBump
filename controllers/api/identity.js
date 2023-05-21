5adc7ca93cfc939fb536220c4a7c7b0312754cc5
julien wagentrutz <julienwagentrutz@MacBook-Pro-de-julien.local>
2023-05-21 19:42:19 +0200

    Update api for vercel

--- controllers/api/identity.js
+++ controllers/api/identity.js
@@ -21,7 +21,7 @@ export const identityController = {
 			{role: "user", content: `Voici les questions et réponses du clients à un formulaire qu'on lui à envoyé pour défénir sa/son ${item.id} : \n ${data}`},
 			{role: "user", content: question}
 		])
-		res.status(200).json(reponse);
+		res.status(200).json(response);
 	},
 }
 
